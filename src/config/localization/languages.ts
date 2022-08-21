ea52e39b1d876eaa1de259d0f08ed4edaf6deef0
localhost3000 <localhost3000too@proton.me>
2022-08-21 08:12:46 +0200

    lang updates

--- src/config/localization/languages.ts
+++ src/config/localization/languages.ts
@@ -2,7 +2,7 @@ import { Language } from '@pancakeswap/uikit'
 
 export const EN: Language = { locale: 'en-US', language: 'English', code: 'en' }
 export const DE: Language = { locale: 'de-DE', language: 'Deutsch', code: 'de' }
-export const ZHCN: Language = { locale: 'zh-CN', language: '简体中文', code: 'zh-cn' }
+export const ZHCN: Language = { locale: 'zh-CN', language: '简体中文', code: 'cn' }
 
 export const languages = {
     'en-US': EN,
